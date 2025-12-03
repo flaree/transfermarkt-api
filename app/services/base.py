855836f40c21ecd9b8adefe00b5468ce8546fd5e
flaree <flare2399@gmail.com>
2025-12-03 12:46:37 +0000

    upd

--- app/services/base.py
+++ app/services/base.py
@@ -49,6 +49,10 @@ class TransfermarktBase:
         try:
             response = httpx.get(
                 url=url,
+                headers={
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+                },
+                timeout=15.0,
             )
             print(f"Requesting URL: {url} - Status Code: {response.status_code}")
         except TooManyRedirects:
